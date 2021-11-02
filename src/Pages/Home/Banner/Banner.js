1c35d0c3011b3d8b921b8657162035f443027442
Md. Ahanaf Tahmid Islame <tahmid231@gmail.com>
2021-11-02 14:25:31 +0600

    home page banner, appointment banner, services done

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,57 @@
+import React from "react";
+import chair from "../../../images/chair.png";
+import bg from "../../../images/bg.png";
+import Grid from "@mui/material/Grid";
+import { Button, Typography, Container, Box } from "@mui/material";
+
+const bannerBg = {
+  background: `url(${bg})`,
+  marginTop: 12,
+};
+const verticalCenter = {
+  display: "flex",
+  alignItems: "center",
+  height: 450,
+};
+const Banner = () => {
+  return (
+    <div style={bannerBg}>
+      <Container sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid
+            item
+            style={{ ...verticalCenter, textAlign: "left" }}
+            xs={12}
+            md={6}
+          >
+            <Box>
+              <Typography variant="h3">
+                Your New Smile <br />
+                Starts Here
+              </Typography>
+              <Typography
+                variant="h6"
+                sx={{ my: 3, fontSize: 13, fontWeight: 300, color: "gray" }}
+              >
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Molestiae eius molestias minima commodi necessitatibus
+                repellendus impedit fugiat a id quisquam!
+              </Typography>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "#5CE7ED" }}
+              >
+                Get Appointment
+              </Button>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6} style={verticalCenter}>
+            <img style={{ width: "350px" }} src={chair} alt="" />
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
